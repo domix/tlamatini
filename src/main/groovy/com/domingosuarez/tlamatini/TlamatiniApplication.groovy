fbdbfcd745306fea5d93148ba862cb3cadb30596
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-07-08 22:24:44 -0500

    initial import

---
+++ src/main/groovy/com/domingosuarez/tlamatini/TlamatiniApplication.groovy
@@ -0,0 +1,12 @@
+package com.domingosuarez.tlamatini
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TlamatiniApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run TlamatiniApplication, args
+  }
+}
---
+++ src/test/groovy/com/domingosuarez/tlamatini/TlamatiniApplicationTests.groovy
@@ -0,0 +1,18 @@
+package com.domingosuarez.tlamatini
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = TlamatiniApplication)
+@WebAppConfiguration
+class TlamatiniApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
